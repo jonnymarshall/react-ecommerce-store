c4820927cb96b40f05457588c1aa9fae2829b1c3
Jonny Marshall <jonnymarshall5@gmail.com>
2020-06-18 14:22:39 +0100

    added shop page & collection preview/item component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import Homepage from './pages/homepage/homepage.component'
 import { Route, Switch } from 'react-router-dom'
+import ShopPage from './pages/shop/shop.component'
 
 function App() {
   return (
     <React.Fragment>
       <Switch>
         <Route exact path='/' component={Homepage}></Route>
+        <Route path='/shop' component={ShopPage}></Route>
       </Switch>
     </React.Fragment>
   );
