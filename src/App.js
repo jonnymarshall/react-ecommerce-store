dc3f5e1861c2af2f27d98d22974f2b2bb712bbbb
Jonny Marshall <jonnymarshall5@gmail.com>
2020-06-18 15:14:33 +0100

    created sign in sign up components

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Homepage from './pages/homepage/homepage.component'
 import { Route, Switch } from 'react-router-dom'
 import ShopPage from './pages/shop/shop.component'
 import Header from './components/header/header.component'
+import SignInSignUp from './pages/sign-in-sign-up/sign-in-sign-up.component'
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Switch>
         <Route exact path='/' component={Homepage}></Route>
         <Route path='/shop' component={ShopPage}></Route>
+        <Route path='/signin' component={SignInSignUp}></Route>
       </Switch>
     </React.Fragment>
   );
